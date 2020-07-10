ab8942f1b7add1305eedead9c27403c9d5ba862f
Gokhan Geyik <geyik@hallowelt.com>
2020-07-10 11:29:24 +0200

    Version 3.1.8

--- includes/misc/pingback/pingback.js
+++ includes/misc/pingback/pingback.js
@@ -1,6 +1,6 @@
 var page = require('webpage').create();
 page.customHeaders = {
-	"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) HalloWelt-dfd/3.1.7",
+	"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) HalloWelt-dfd/3.1.8",
 	"Accept-Language": 'en;q=0.8'
 };
 
