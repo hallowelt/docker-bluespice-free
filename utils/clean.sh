c802e078d986d51a4568858716e32143e3d5b94a
jonty <jontykantiwal@gmail.com>
2023-12-18 12:40:18 +0100

    fixed venv and wget not working and install python deps

--- utils/clean.sh
+++ utils/clean.sh
@@ -9,7 +9,7 @@ NORMAL=$(tput sgr0)
 if [ "$VIRTUAL_ENV" != "" ]; then
     echo "${BOLD}${RED}Exit venv first by ctrl+d or exit${NC}${NORMAL}"
 else
-    rm -rf venv
+    rm -rf venv bs_venv
     find -iname "*.pyc" -delete
     echo "${BOLD}${GREEN}======= Successfully cleaned =======${NC}${NORMAL}"
 fi
--- utils/install.sh
+++ utils/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 RED=$(tput setaf 1)
 GREEN=$(tput setaf 2)
 MAGENTA=$(tput setaf 5)
@@ -9,6 +11,8 @@ BOLD=$(tput bold)
 NORMAL=$(tput sgr0)
 INVENV=$(python3 -c 'import sys; print ("1" if hasattr(sys, "real_prefix") else "0")')
 VENV_DIR=./bs_venv
+
+sudo apt install python3-venv python3-pip -y
 
 if [ -d $VENV_DIR ] && [ $INVENV == "0" ]; then
     echo "Found $VENV_DIR, activating it: "
